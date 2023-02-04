afd649c2258724575d6730e914837978dc1f97ba
Alejandro Soto <45957069+HAlejandro88@users.noreply.github.com>
2023-02-03 21:15:48 -0600

    Create do-while.js

---
+++ do-while.js
@@ -0,0 +1,14 @@
+
+const carros = ['Honda', 'Ford', 'Jeep', 'Kia', 'Toyota' ]
+
+let i = 0;
+
+
+do {
+  // este codigo solo se ejecuta una vez
+} while()
+
+while(i < carros.lenght) {
+  console.log(carros[i])
+  i++
+}
